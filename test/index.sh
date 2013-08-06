f388623bd4867b46c3a3c9c08de3f96c11086d07
Artem Polikarpov <artpolikarpov@gmail.com>
2013-08-06 15:51:01 +0400

    Tests listing

--- test/index.sh
+++ test/index.sh
@@ -2,8 +2,8 @@
 
 for DIR in $(find ./test -type d); do
   (
-    echo -e "<html>\n<body>\n<h1>Manual tests</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
-    echo -e "</pre>\n</body>\n</html>"
+    echo -e "<html>\n<body style=\"line-height: 1.6;\">\n<h1>Manual tests</h1>\n<ul>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<li><a href=\"%s\">%s</a></li>\n",$1,$1 }'
+    echo -e "</ul>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
