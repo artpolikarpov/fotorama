3fa6ea737446745748a2e808c07c0d9672e741f1
Artem Polikarpov <artpolikarpov@gmail.com>
2013-09-09 17:56:56 +0400

    (Ready for 4.3.4.2)

--- src/js/intro.js
+++ src/js/intro.js
@@ -5,4 +5,4 @@
 
   if (!JQUERY_VERSION
       || JQUERY_VERSION[0] < 1
-      || (JQUERY_VERSION[0] == 1 && JQUERY_VERSION[1] < 8)) return console.error('Fotorama requires jQuery 1.8 or later and will not run without it.');
+      || (JQUERY_VERSION[0] == 1 && JQUERY_VERSION[1] < 8)) return console['error']('Fotorama requires jQuery 1.8 or later and will not run without it.');
