7c60f7b71fc7a863847e796b2c6d6d00264ac90d
Artem Polikarpov <artpolikarpov@gmail.com>
2013-09-14 22:20:33 +0400

    (`fotorama--hidden-before-ready`)

--- src/js/skip.js
+++ src/js/skip.js
@@ -4,8 +4,5 @@ if (!JQUERY_VERSION
     || JQUERY_VERSION[0] < 1
     || (JQUERY_VERSION[0] == 1 && JQUERY_VERSION[1] < 8)) {
   window.console && console['error']('Fotorama requires jQuery 1.8 or later and will not run without it.');
-  $ && $(function () {
-    $('.' + _fotoramaClass).addClass(_fotoramaClass + '--noscript')
-  })();
   return;
 }
