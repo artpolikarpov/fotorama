3a5b93333344aaaf55f68bc22b412028bf096edc
Artem Polikarpov <artpolikarpov@gmail.com>
2013-09-19 08:01:11 +0400

    (Recompile indexes of manual tests for gh-pages)

--- test/index.sh
+++ test/index.sh
@@ -2,7 +2,7 @@
 
 for DIR in $(find ./test -type d); do
   (
-    echo -e "<html>\n<body style=\"line-height: 1.6;\">\n<h1>Index of ${DIR}</h1>\n<ul>"
+    echo -e "<html>\n<head><meta name=\"viewport\" content=\"width=device-width\"></head>\n\n<body style=\"line-height: 1.6;\">\n<h1>Index of ${DIR}</h1>\n<ul>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<li><a href=\"%s\">%s</a></li>\n",$1,$1 }'
     echo -e "</ul>\n</body>\n</html>"
   ) > "${DIR}/index.html"
