0369e2638222fdecba5bb130f3a24d5169579750
Artem Polikarpov <artpolikarpov@gmail.com>
2013-09-21 23:21:11 +0400

    No tap if moved

--- src/js/moveontouch.js
+++ src/js/moveontouch.js
@@ -149,7 +149,7 @@ function moveOnTouch ($el, options) {
 
     time *= slowFLAG ? 10 : 1;
 
-    (options.onEnd || noop).call(el, $.extend(result, {pos: moveElPos, newPos: newPos, overPos: overPos, time: time, moved: longTouchFLAG ? snap : Math.abs(moveElPos - startElPos) > 3}));
+    (options.onEnd || noop).call(el, $.extend(result, {pos: moveElPos, newPos: newPos, overPos: overPos, time: time, moved: longTouchFLAG ? snap : Math.abs(moveElPos - startElPos) > (snap ? 0 : 3)}));
   }
 
   tail = $.extend(touch(options.$wrap, {
