c91915e09635a6c38b34a61d4032798be3ba97e2
Artem Polikarpov <artpolikarpov@gmail.com>
2014-08-03 15:11:25 +0400

    Delete unnecessary `return` from skip.js

--- src/js/skip.js
+++ src/js/skip.js
@@ -4,5 +4,4 @@ if (!JQUERY_VERSION
     || JQUERY_VERSION[0] < 1
     || (JQUERY_VERSION[0] == 1 && JQUERY_VERSION[1] < 8)) {
   throw 'Fotorama requires jQuery 1.8 or later and will not run without it.';
-  return;
 }
