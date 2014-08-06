bf11cfd7f1d921c9237645a85bd48370093ddf62
Artem Polikarpov <artpolikarpov@gmail.com>
2014-08-06 23:25:54 +0400

    Rebuild #4.5.3-beta

--- out/fotorama.dev.js
+++ out/fotorama.dev.js
@@ -3656,7 +3656,7 @@ $(function () {
   $('.' + _fotoramaClass + ':not([data-auto="false"])').fotorama();
 
   // Get usage statistics
-  if (getProtocol().p !== 'http://' || !location.host.match(/\./) || window.blockFotoramaData) return;
+  if (getProtocol() !== 'http://' || !location.host.match(/\./) || window.blockFotoramaData) return;
   $('body').append('<iframe src="http://data.fotorama.io/?version=' + fotoramaVersion + '" style="display: none;"></iframe>');
 });
 })(window, document, location, typeof jQuery !== 'undefined' && jQuery);
