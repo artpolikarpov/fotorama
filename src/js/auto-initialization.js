045c81c99c904f9ebcfa71c8889ee72a95046d73
Artem Polikarpov <artpolikarpov@gmail.com>
2014-08-06 23:12:00 +0400

    Fix auto-initialization.js

--- src/js/auto-initialization.js
+++ src/js/auto-initialization.js
@@ -2,6 +2,6 @@ $(function () {
   $('.' + _fotoramaClass + ':not([data-auto="false"])').fotorama();
 
   // Get usage statistics
-  if (getProtocol().p !== 'http://' || !location.host.match(/\./) || window.blockFotoramaData) return;
+  if (getProtocol() !== 'http://' || !location.host.match(/\./) || window.blockFotoramaData) return;
   $('body').append('<iframe src="http://data.fotorama.io/?version=' + fotoramaVersion + '" style="display: none;"></iframe>');
 });
