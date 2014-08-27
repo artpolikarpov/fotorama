d12531ef2558f06d9d90ecf39b2032178e38e26f
Artem Polikarpov <artpolikarpov@gmail.com>
2014-08-27 13:31:09 +0400

    (Update Gruntfile.js)

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,31 +153,6 @@ copy: {
         dest: '.fotorama-npm/dist/fotorama.js'
       }
     ]
-  },
-  cdnjs: {
-    path: '/Users/artpolikarpov/Projects/Clone/cdnjs/ajax/libs/fotorama',
-    files: [
-      {
-        src: 'out/fotorama.css',
-        dest: '<%= copy.cdnjs.path %>/<%= pkg.version %>/fotorama.css'
-      },
-        {
-        src: 'out/fotorama.png',
-        dest: '<%= copy.cdnjs.path %>/<%= pkg.version %>/fotorama.png'
-      },
-      {
-        src: 'out/fotorama@2x.png',
-        dest: '<%= copy.cdnjs.path %>/<%= pkg.version %>/fotorama@2x.png'
-      },
-      {
-        src: 'out/fotorama.js',
-        dest: '<%= copy.cdnjs.path %>/<%= pkg.version %>/fotorama.js'
-      },
-      {
-        src: 'fotorama.jquery.json',
-        dest: '<%= copy.cdnjs.path %>/package.json'
-      }
-    ]
   }
 },
 concat: {
@@ -392,13 +367,6 @@ shell: {
   indexes: {
     command: './test/index.sh'
   },
-  cdnjs: {
-    command: 'cd ~/Projects/Clone/cdnjs/ ' +
-        '&& git add . ' +
-        '&& git commit -am \'Update Fotorama to <%= pkg.version %>\' ' +
-        '&& git pull cdnjs master ' +
-        '&& git push --progress origin master:master'
-  },
   commit: {
     command: 'git commit fotorama.jquery.json -m \'Tagging the <%= pkg.version %> release\''
   },
@@ -494,9 +462,6 @@ grunt.registerTask('default', defaultTask.split(' '));
 //grunt.registerTask('look', 'copy:i sass autoprefixer jst replace:jst concat:js watch'.split(' '));
 
 // Publish, will fail without secret details ;-)
-grunt.registerTask('cdnjs', (defaultTask + ' s3 replace:version copy:cdnjs shell:cdnjs').split(' '));
-
-// Publish, will fail without secret details ;-)
-grunt.registerTask('publish', (defaultTask + ' s3 replace:version copy:bower shell:commit shell:push shell:bower shell:npm shell:heroku').split(' '));
+grunt.registerTask('publish', (defaultTask + ' s3 replace:version copy:bower copy:npm shell:commit shell:push shell:bower shell:npm shell:heroku').split(' '));
 grunt.registerTask('release', ('replace:history gh_release tweet').split(' '));
 };
