00cb4dca9e4c60d81043702849e4740381c1fa15
Nate Tovo <nate.tovo@gmail.com>
2014-09-30 15:57:13 -0500

    correcting style.jst to use the generated js fotorama class names

--- src/js/fotorama.js
+++ src/js/fotorama.js
@@ -301,7 +301,17 @@ jQuery.Fotorama = function ($fotorama, opts) {
       $navFrame = $navThumbFrame;
       navFrameKey = NAV_THUMB_FRAME_KEY;
 
-      setStyle($style, $.Fotorama.jst.style({w: o_thumbSide, h: o_thumbSide2, b: opts.thumbborderwidth, m: opts.thumbmargin, s: stamp, q: !COMPAT}));
+      setStyle($style, $.Fotorama.jst.style({
+        w: o_thumbSide, 
+        h: o_thumbSide2, 
+        b: opts.thumbborderwidth, 
+        m: opts.thumbmargin, 
+        s: stampClass,
+        nt: navThumbsClass,
+        nf: navFrameClass,
+        tb: thumbBorderClass,
+        q: !COMPAT
+      }));
 
       $nav
           .addClass(navThumbsClass)
