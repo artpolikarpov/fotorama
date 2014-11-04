fb4145f814fa1b4b25471b0c121d151e306237e4
Artem Polikarpov <artpolikarpov@gmail.com>
2014-11-04 13:40:28 +0300

    No more “phoning home”

--- src/js/auto-initialization.js
+++ src/js/auto-initialization.js
@@ -1,7 +1,3 @@
 $(function () {
   $('.' + _fotoramaClass + ':not([data-auto="false"])').fotorama();
-
-  // Get usage statistics
-  if (getProtocol() !== 'http://' || !location.host.match(/\./) || window.blockFotoramaData) return;
-  $('body').append('<iframe src="http://data.fotorama.io/?version=' + fotoramaVersion + '" style="display: none;"></iframe>');
 });
