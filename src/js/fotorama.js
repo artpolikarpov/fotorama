fa8d8dc27b93d0879a63cb88501d7ec67f7ad8e0
svetoslavatanasov <svetoslavatanasov@users.noreply.github.com>
2015-02-18 11:03:29 +0200

    Update fotorama.js
    More flexible stage dimensions

--- src/js/fotorama.js
+++ src/js/fotorama.js
@@ -1239,7 +1239,7 @@ jQuery.Fotorama = function ($fotorama, opts) {
     var width = measures.width,
         height = measures.height,
         ratio = measures.ratio,
-        windowHeight = $WINDOW.height() - (o_nav ? $nav.height() : 0);
+        windowHeight = $WINDOW.height() - (o_nav && $navWrap.css('position') != 'absolute' && $navWrap.css('position') != 'fixed' ? $nav.height() : 0);
 
     if (measureIsValid(width)) {
       $wrap
